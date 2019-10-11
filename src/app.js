f1b99337965fb35d69b779d2d71713ea14648ca2
Angel Manuel Goez Giraldo <angel.goez@da.arus.com.co>
2019-10-11 17:44:13 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '..', 'public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    res.send('weather');
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})

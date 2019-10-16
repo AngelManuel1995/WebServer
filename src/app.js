a59f490bec9f51022e740ef2f29a9026b1406d1b
Angel Manuel Goez Giraldo <angel.goez@da.arus.com.co>
2019-10-16 18:41:16 -0500

    view folder added and hbs installed

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ const path = require('path')
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '..', 'public')
-
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title:'Weather App',
+        name:'Angel Manuel Góez Giraldo',
+        items: ['Item 1','Item 2','Item 3','Item 4','Item 5']
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'About Page',
+        items: ['Item 1','Item 2','Item 3','Item 4','Item 5']
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('about', {
+        title:'Help Page'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send('weather');
